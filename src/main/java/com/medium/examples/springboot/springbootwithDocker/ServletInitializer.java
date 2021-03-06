1a5046d519e0b7ab0694723d18c1059432d7c5a7
ren <renjanmenon@gmail.com>
2021-03-05 22:38:36 -0700

    feat(init) : initial commit

---
+++ src/main/java/com/medium/examples/springboot/springbootwithDocker/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.medium.examples.springboot.springbootwithDocker;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootwithDockerApplication.class);
+	}
+
+}
---
+++ src/main/java/com/medium/examples/springboot/springbootwithDocker/SpringbootwithDockerApplication.java
@@ -0,0 +1,13 @@
+package com.medium.examples.springboot.springbootwithDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwithDockerApplication.class, args);
+	}
+
+}
